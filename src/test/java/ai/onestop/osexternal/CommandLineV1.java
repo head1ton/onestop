b234920f3f94ddf14319ffea600f8507db1a3025
head1ton <head1ton@gmail.com>
2023-09-23 20:49:58 +0900

    CommandLine

---
+++ src/test/java/ai/onestop/osexternal/CommandLineV1.java
@@ -0,0 +1,13 @@
+package ai.onestop.osexternal;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class CommandLineV1 {
+
+    public static void main(String[] args) {
+        for (String arg : args) {
+            log.info("arg: {}", arg);
+        }
+    }
+}
