5a9aac1cdabe6b96fc82d4a9523ac3952c05d712
head1ton <head1ton@gmail.com>
2023-09-24 20:07:55 +0900

    reset

--- src/main/java/ai/onestop/command/CommandLineBean.java
+++ src/main/java/ai/onestop/command/CommandLineBean.java
@@ -8,7 +8,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
+//@Component
 public class CommandLineBean {
 
     private final ApplicationArguments arguments;
