1b464da4878ce039d1a5f407615fb83a1cc32fec
head1ton <head1ton@gmail.com>
2023-09-25 11:18:00 +0900

    logging

--- src/main/java/ai/onestop/OnestopApplication.java
+++ src/main/java/ai/onestop/OnestopApplication.java
@@ -1,13 +1,20 @@
 package ai.onestop;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnestopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnestopApplication.class, args);
+    }
+
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
--- src/main/java/ai/onestop/env/EnvironmentCheck.java
+++ src/main/java/ai/onestop/env/EnvironmentCheck.java
@@ -6,7 +6,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
+//@Component
 public class EnvironmentCheck {
 
     private final Environment env;
